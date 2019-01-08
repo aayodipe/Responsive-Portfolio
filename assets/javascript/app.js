ac91d7a8c97c43bc049efc84d7a85426851ec6f6
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-01-08 17:24:51 -0600

    added the jquery links to all the file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,136 +1,136 @@
 const animals = [
-			'Elephant',
-			'Eagle',
-			'Fish',
-			'Lion',
-			'Dog',
-			'Leopard',
-			'Bat',
-			'Cat',
-			'Monkey',
-			'Bongo',
-			'Python',
-			'Deer',
-			'Whales',
-			'Wolf',
-			'Kangaroo',
-			'sheep',
-			'Camel'
-		];
+	'Elephant',
+	'Eagle',
+	'Fish',
+	'Lion',
+	'Dog',
+	'Leopard',
+	'Bat',
+	'Cat',
+	'Monkey',
+	'Bongo',
+	'Python',
+	'Deer',
+	'Whales',
+	'Wolf',
+	'Kangaroo',
+	'sheep',
+	'Camel'
+];
 
-		//render html
-		function renderAnimal() {
-			//loop through the animal array to render the populate the page with the animal
-			animals.forEach(animal => {
-				//creates button for each animal in the array
-				let item = $('<button>');
-				item.addClass('animal')
-					.attr('id', animal)
-					.css({
-						backgroundColor: 'blue',
-						color: 'white',
-						margin: '10px',
-						width: '150px',
-						padding: '10px',
-						fontSize: '20px',
+//render html
+function renderAnimal() {
+	//loop through the animal array to render the populate the page with the animal
+	animals.forEach(animal => {
+		//creates button for each animal in the array
+		let item = $('<button>');
+		item.addClass('animal')
+			.attr('id', animal)
+			.css({
+				backgroundColor: 'blue',
+				color: 'white',
+				margin: '10px',
+				width: '150px',
+				padding: '10px',
+				fontSize: '20px',
 
-					})
-					//apply hover color to the button
-					.hover(function (e) {
-						$(this).css('background-color', e.type === 'mouseenter' ? 'Teal' : 'blue');
-					})
+			})
+			//apply hover color to the button
+			.hover(function (e) {
+				$(this).css('background-color', e.type === 'mouseenter' ? 'Teal' : 'blue');
+			})
 
-					.text(animal)
-					//append the animals to the page
-					.appendTo('#animal-buttons');
-			});
-		}
+			.text(animal)
+			//append the animals to the page
+			.appendTo('#animal-buttons');
+	});
+}
 
-		$('#input-button').on('click', function (event) {
-			//prevent browser default
-			event.preventDefault();
-			//empty the animal array
+$('#input-button').on('click', function (event) {
+	//prevent browser default
+	event.preventDefault();
+	//empty the animal array
+	$('#animal-buttons').empty();
+	let animalAdded = $('#animal-input')
+		.val()
+		.trim();
+	//check animal added is already exist or nothing is typed in the input box
+	for (var i = 0; i < animals.length; i++) {
+		if (animals.includes(animalAdded) || animalAdded.length === 0) {
+			$('#animal-input').val('');
 			$('#animal-buttons').empty();
-			let animalAdded = $('#animal-input')
-				.val()
-				.trim();
-			//check animal added is already exist or nothing is typed in the input box
-			for (var i = 0; i < animals.length; i++) {
-				if (animals.includes(animalAdded) || animalAdded.length === 0) {
-					$('#animal-input').val('');
-					$('#animal-buttons').empty();
-					renderAnimal();
-					return false;
-				} else {
-					//test
-					console.log('Animal Added: ' + animalAdded);
-					animals.push(animalAdded);
-					renderAnimal();
-					$('#animal-input').val('');
-				}
-			}
-		});
-		renderAnimal();
-
-		$(document.body).on('click', '.animal', function () {
-			//giphy api key
-			const apiKey = 'vL5PfNP79JhSnylWQQIL9q21wNGcdgd5';
-			//stores name of animal clicked
-			let animalPick = $(this).attr('id');
+			renderAnimal();
+			return false;
+		} else {
 			//test
-			console.log('Animal Picked: ' + animalPick);
+			console.log('Animal Added: ' + animalAdded);
+			animals.push(animalAdded);
+			renderAnimal();
+			$('#animal-input').val('');
+		}
+	}
+});
+renderAnimal();
 
-			//get giphy url
-			let queryURL =
-				'https:api.giphy.com/v1/gifs/search?api_key=' +
-				apiKey +'&q=' + animalPick +'&limit=10&offset=0&rating=G&lang=en';
+$(document.body).on('click', '.animal', function () {
+	//giphy api key
+	const apiKey = 'vL5PfNP79JhSnylWQQIL9q21wNGcdgd5';
+	//stores name of animal clicked
+	let animalPick = $(this).attr('id');
+	//test
+	console.log('Animal Picked: ' + animalPick);
 
-			// Performing our AJAX GET request
-			$.ajax({
-					url: queryURL,
-					method: 'GET',
-				})
-				// After the data comes back from the API
-				.then(function (response) {
-					// Storing an array of results in the results variable
-					var results = response.data;
-					console.log(response)
+	//get giphy url
+	let queryURL =
+		'https:api.giphy.com/v1/gifs/search?api_key=' +
+		apiKey + '&q=' + animalPick + '&limit=10&offset=0&rating=G&lang=en';
 
-					// Looping over every result item
-					results.forEach(animal =>{
+	// Performing our AJAX GET request
+	$.ajax({
+			url: queryURL,
+			method: 'GET',
+		})
+		// After the data comes back from the API
+		.then(function (response) {
+			// Storing an array of results in the results variable
+			var results = response.data;
+			console.log(response)
 
-					
-						// Only taking action if the photo has an appropriate rating
-						if (animal.rating !== 'r' && animal.rating !== 'pg-13') {
-							// Creating a div for the gif
-							var gifDiv = $('<div>')
-							gifDiv.css({
-								border: '2px solid blue',
-								color: 'black',
-								margin: '3px',
-								float: 'left'
-							});
+			// Looping over every result item
+			results.forEach(animal => {
 
-							// Storing the result item's rating
-							var rating = animal.rating;
 
-							// Creating a paragraph tag with the result item's rating
-							var p = $('<p>').text('Rating: ' + rating);
+				// Only taking action if the photo has an appropriate rating
+				if (animal.rating !== 'r' && animal.rating !== 'pg-13') {
+					// Creating a div for the gif
+					var gifDiv = $('<div>')
+					gifDiv.css({
+						border: '2px solid blue',
+						color: 'black',
+						margin: '3px',
+						float: 'left'
+					});
 
-							// Creating an image tag
-							var animalImage = $('<img>');
+					// Storing the result item's rating
+					var rating = animal.rating;
 
-							// Giving the image tag an src attribute of a proprty pulled off the
-							// result item
-							animalImage.attr('src', animal.images.fixed_height.url);
+					// Creating a paragraph tag with the result item's rating
+					var p = $('<p>').text('Rating: ' + rating);
 
-							// Appending the paragraph and personImage we created to the "gifDiv" div we created
-							gifDiv.append(p);
-							gifDiv.append(animalImage);
+					// Creating an image tag
+					var animalImage = $('<img>');
 
-							// Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-							$('#images').prepend(gifDiv);
-						}
-					}) 
-				});
+					// Giving the image tag an src attribute of a proprty pulled off the
+					// result item
+					animalImage.attr('src', animal.images.fixed_height.url);
+
+					// Appending the paragraph and personImage we created to the "gifDiv" div we created
+					gifDiv.append(p);
+					gifDiv.append(animalImage);
+
+					// Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+					$('#images').prepend(gifDiv);
+				}
+			})
 		});
+});
