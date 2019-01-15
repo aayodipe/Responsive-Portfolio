2c56e75ed122161ddbabe9c53fa371c6313bb4f0
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-01-15 00:04:54 -0600

    updated the trainapp.js

--- assets/javascript/trainapp.js
+++ assets/javascript/trainapp.js
@@ -1,76 +1,76 @@
-$(document).ready(function(){
-//Initialize Firebase
-var config = {
-     apiKey: "AIzaSyAY5gwnrVbokOMDTLedvL1YCnA_RKdtwkA",
-     authDomain: "fr-demo-app-2.firebaseapp.com",
-     databaseURL: "https://fr-demo-app-2.firebaseio.com",
-     projectId: "fr-demo-app-2",
-     storageBucket: "fr-demo-app-2.appspot.com",
-     messagingSenderId: "1029611183819"
-};
-// Initialize Firebase
-firebase.initializeApp(config);
+$(document).ready(function () {
+     //Initialize Firebase
+     var config = {
+          apiKey: "AIzaSyAY5gwnrVbokOMDTLedvL1YCnA_RKdtwkA",
+          authDomain: "fr-demo-app-2.firebaseapp.com",
+          databaseURL: "https://fr-demo-app-2.firebaseio.com",
+          projectId: "fr-demo-app-2",
+          storageBucket: "fr-demo-app-2.appspot.com",
+          messagingSenderId: "1029611183819"
+     };
+     // Initialize Firebase
+     firebase.initializeApp(config);
 
-// Create a variable to reference the database
-let database = firebase.database()
+     // Create a variable to reference the database
+     let database = firebase.database()
 
-//timing
-let startTime = moment().format("hh:mm");
-let trentonTimer = moment().add(1000000, 'milliseconds')
-let OregonTimer = startTime+3600
-
-
-console.log(startTime)
-
-let trainName = "";
-let destination ="";
-let trainTime = "";
-let frequency = "";
-let nameCell = "";
-let destinationCell="";
-let frequencyCell ="";
-let trainTimeCell = ""; 
-
-// Button for adding New Train
-$(".submit").on("click", function (event) {
-  //Prevent default behaviour of submit event
-     event.preventDefault()
-   // grab user Input
-     trainName = $("#train-name").val().trim()
-     destination = $("#destination").val().trim()
-     trainTime = $("#train-time").val().trim()
-     frequency = $("#frequency").val().trim()
- 
-
-     console.log(trainName, destination, trainTime, frequency)
+     //timing
+     let startTime = moment().format("hh:mm");
+     let trentonTimer = moment().add(1000000, 'milliseconds')
+     let OregonTimer = startTime + 3600
 
 
-     database.ref().push({
-          Train_Name: trainName,
-          Destination: destination,
-          Train_Time: trainTime,
-          Frequency: frequency,
-        
+     console.log(startTime)
+
+
+
+     // Button for adding New Train
+     $(".submit").on("click", function (event) {
+          //Prevent default behaviour of submit event
+          event.preventDefault()
+          // grab user Input
+          let trainName = $("#train-name").val().trim()
+          let destination = $("#destination").val().trim()
+          let trainTime = $("#train-time").val().trim()
+          let frequency = $("#frequency").val().trim()
+
+
+          console.log(trainName, destination, trainTime, frequency)
+
+          let trainData = {
+               Train_Name: trainName,
+               Destination: destination,
+               Train_Time: trainTime,
+               Frequency: frequency,
+
+          }
+          // Uploads employee data to the database
+          database.ref().push(trainData)
+
+          //clear the input field
+          $("#train-name").val("")
+          $("#destination").val("")
+          $("#train-time").val("")
+          $("#frequency").val("")
      })
 
+     database.ref().on("child_added", function (snapshot) {
+         // Store everything into a variable.
+         let tName = snapshot.val().Train_Name
+         let tDestination = snapshot.val().Destination
+         let tFrequency = snapshot.val().Frequency
+         let tTime = snapshot.val().Train_Time
 
-database.ref().on("value", function (snapshot) {
-     console.log(snapshot.val());
-
-     let tr = $('<tr>')
-      nameCell = $("<td>").text(snapshot.val().Train_Name)
-     nameCell.appendTo(tr)
-     tr.appendTo("tbody")
-      destinationCell = $("<td>").text(snapshot.val().Destination)
-     destinationCell.appendTo(tr)
+         let tr = $("<tr>").append(
+          $("<td>").text(tName),
+          $("<td>").text(tDestination),
+          $("<td>").text(tFrequency),
+          $("<td>").text(tTime)
+         )
+          tr.appendTo("tbody")
+           // Handle the errors
+     }, function(errorObject) {
+          console.log("Errors handled: " + errorObject.code);
+        });
     
-      frequencyCell = $("<td>").text(snapshot.val().Frequency)
-     frequencyCell.appendTo(tr)
-    
-      trainTimeCell = $("<td>").text(snapshot.val().Train_Time)
-     trainTimeCell.appendTo(tr);
-     dateAdded: firebase.database.ServerValue.TIMESTAMP
-     
-})
-})
 })
